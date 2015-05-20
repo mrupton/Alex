3cc6ee51f2080d88d3e28d2199c9ff35a19d97d7
mrupton <emine.m@aol.com>
2015-05-20 14:03:14 -0400

    Update Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-Using System.LeagueSharp;
+using LeagueSharp;
 
-namespace ConsoleApplication1
+namespace AlexisME
 {
     class Program
     {
